a164fa09da68cb5c60a8afbea11166249dc6b473
Md-arefin <ansariarefin617@gmail.com>
2024-06-04 11:13:15 +0300

    added new code

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,55 @@
 /* eslint-disable react/prop-types */
+import app from "../Firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { createContext, useEffect, useState } from "react";
 
-
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading , setLoading] = useState(false);
+    
+    // create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // signIn with Email and Password
+    const signIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            console.log("currentUser", currentUser);
+            setLoading(false);
+        });
+
+        return ()=>{
+            return unsubscribe();
+        };
+        
+    },[]);
+
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        signIn,
+    }
+
     return (
-        <div>
+        <AuthContext.Provider value={authInfo}>
             {children}
-        </div>
+        </AuthContext.Provider>
     );
 };
 
--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,10 +1,68 @@
+import { useForm } from "react-hook-form";
+import { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 
 const Login = () => {
+
+    
+    const { signInWithGoogle, signIn } = useContext(AuthContext);
+    const { register, handleSubmit, reset } = useForm();
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+
     return (
-        <div>
-            this is login page
+        <div className='flex flex-col md:flex-row mx-4 gap-10'>
+        <div className='w-full md:w-1/3 mx-auto md:mt-36'>
+            <img src="" alt="" />
         </div>
+
+        <form className='w-full md:w-1/3 mx-auto md:mt-24 p-2 md:p-5 lg:p-10 border-2 rounded-lg border-black ' onSubmit="">
+
+            <h5 className='text-center font-semibold text-3xl'>Please Login</h5>
+
+            <div className="flex flex-col gap-3  mb-2">
+                <label htmlFor="email">Email</label>
+                <input
+                    className="w-full rounded-md"
+                    type="email"
+                    placeholder='Enter your email...'
+                    {...register("email")} required />
+            </div>
+
+            <div className="flex flex-col gap-3  mb-2">
+                <label htmlFor="password">Password</label>
+                <input
+                    className="w-full rounded-md"
+                    type="password"
+                    placeholder='Enter your password...'
+                    {...register("password")} required />
+            </div>
+
+            <p className='text-center'>New to Travelling? <Link to='/sign-up' className='underline font-semibold'> Please Sign-up</Link></p>
+
+            <div className="flex justify-center my-10">
+
+                <button type="submit" className="bg-[#a98467] btn w-1/2 p-1 rounded-md border-2 text-white border-black flex justify-evenly items-center">Submit </button>
+            </div>
+
+            <div className='border-2 w-3/4 mx-auto my-5'></div>
+
+            <h5 className='text-center font-semibold text-3xl'>Social Login</h5>
+
+            <div className=' flex items-center justify-evenly gap-5 my-5'>
+                <img onClick="" src="" className='w-10 cursor-pointer' alt="" />
+                <img src="" className='w-10 cursor-pointer' alt="" />
+                <img src=''
+                className='w-10 cursor-pointer' alt="" />
+            </div>
+
+        </form>
+
+    </div>
     );
 };
 
